c2236a377f001d24cc94a022d32e93df9d6780e2
Kajal Gupta <165830057+ketchup269@users.noreply.github.com>
2025-08-22 14:11:42 +0000

    updated

--- app/routes/contact/contact.jsx
+++ app/routes/contact/contact.jsx
@@ -30,12 +30,19 @@ const MAX_EMAIL_LENGTH = 512;
 const MAX_MESSAGE_LENGTH = 4096;
 const EMAIL_PATTERN = /(.+)@(.+){2,}\.(.+){2,}/;
 
-
 export async function action({ context, request }) {
+  const ses = new SESClient({
+    region: 'us-east-1',
+    credentials: {
+      accessKeyId: context.cloudflare.env.AWS_ACCESS_KEY_ID,
+      secretAccessKey: context.cloudflare.env.AWS_SECRET_ACCESS_KEY,
+    },
+  });
+
   const formData = await request.formData();
-  const isBot = String(formData.get("name"));
-  const email = String(formData.get("email"));
-  const message = String(formData.get("message"));
+  const isBot = String(formData.get('name'));
+  const email = String(formData.get('email'));
+  const message = String(formData.get('message'));
   const errors = {};
 
   // Return without sending if a bot trips the honeypot
@@ -43,11 +50,11 @@ export async function action({ context, request }) {
 
   // Handle input validation on the server
   if (!email || !EMAIL_PATTERN.test(email)) {
-    errors.email = "Please enter a valid email address.";
+    errors.email = 'Please enter a valid email address.';
   }
 
   if (!message) {
-    errors.message = "Please enter a message.";
+    errors.message = 'Please enter a message.';
   }
 
   if (email.length > MAX_EMAIL_LENGTH) {
@@ -191,7 +198,7 @@ export const Contact = () => {
           </Form>
         )}
       </Transition>
-      <Transition unmount in={actionData?.success}>
+      <Transition unmount in={actionData}>
         {({ status, nodeRef }) => (
           <div className={styles.complete} aria-live="polite" ref={nodeRef}>
             <Heading
